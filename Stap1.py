0665496a2c4137a6dd4301b4f6fee3ee34bc0678
bgmeulem <bjorge.meulemeester@ugent.be>
2018-05-08 15:27:36 +0200

    Cleaning up

--- Stap1.py
+++ Stap1.py
@@ -37,7 +37,6 @@ def BindPotDer2(r):
 
 def aperi(E, L):
     """ De eerste lijnen is het oplossen van de vergelijking uit 1.112 """
-<<<<<<< HEAD
     p = [2*E, 2*E-2, L**2, (L**2)]
     aperi = numpy.roots(p)
     i = 0
@@ -60,34 +59,10 @@ def peri(E, L):
     else:
         return aper[0]
 
+
 def ap(E, L):
     aper = aperi(E, L)
     if aper[0] > aper[1]:
         return aper[0]
     else:
         return aper[1]
-=======
-    p=[2*E, 2*E-2, L**2, (L**2)]
-    aperi = numpy.roots(p)
-    i = 0
-    aperi2=[]
-    """Hier filteren we alle oplossingen die positief zijn, en dus fysisch eruit"""
-    while i<3:
-        if aperi[i]>0:
-            del aperi[i]
-            i = 3
-        i+=1
-    return(aperi)
-    """Vervolgens bepalen we welke aphelium en welke periheliumafstand is"""
-    """Eventueel kan dit herschreven worden in 2 aparte functies, een voor peri een voor aphelium"""
-    if aperi[0]>aperi[1]:
-        aphelium = aperi[0]
-        perihelium = aperi[1]
-    elif aperi[0]==aperi[1]:
-        aphelium = aperi[1]
-        perihelium = aperi[0] 
-    else:
-        aphelium = aperi2[1]
-        perihelium = aperi2[0]
-    return(perihelium, aphelium)
->>>>>>> 0d101ae349d3c751746bef29d75a9c03ac8e079c
